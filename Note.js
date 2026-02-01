34eb7464ab594b763ada0b740aa402130a4b7ef8
Victory Mwendwa <mwendwavictory29@gmail.com>
2026-02-01 22:35:03 +0300

    reorganize project into client-server architecture and initialize vite frontend

--- Note.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const noteSchema = new mongoose.Schema({
-    title: String,
-    content: {type: String, required: true},
-    createdAt: {type:Date, default: Date.now},
-});
-
-module.exports = mongoose.model('Note', noteSchema);
